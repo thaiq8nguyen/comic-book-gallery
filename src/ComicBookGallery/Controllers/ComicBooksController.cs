23a91bbc6833f2a2707f2690b4c4da57596e18c6
Thai Nguyen <thai@thaiqnguyen.com>
2017-04-18 11:15:38 -0700

    add design files

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -16,11 +16,13 @@ namespace ComicBookGallery.Controllers
             ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong>";
             ViewBag.Artists = new string[]
             {
+
                 "Script: Dan Slott",
                 "Pencils: Humberto Ramos",
                 "Inks: Victor Olazaba",
                 "Colors: Edgar Delgado",
                 "Letters: Chris Eliopoulos"
+
             };
             return View();
             
