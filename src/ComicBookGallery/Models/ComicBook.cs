35cca720ca7ddb625da856cac35166c4f4016f9e
Thai Nguyen <thai@thaiqnguyen.com>
2017-04-18 17:27:25 -0700

    update the comic book detail view layout

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -12,7 +12,7 @@ namespace ComicBookGallery.Models
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
-        public bool Favortite { get; set; }
+        public bool Favorite { get; set; }
 
         public string DisplayText
         {
